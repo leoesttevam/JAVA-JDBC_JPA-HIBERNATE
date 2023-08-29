644a9d274c9db2dce71fc2cb74e96a22538e2a13
leoesttevam <leonardoestevam17@gmail.com>
2023-08-29 15:15:48 -0300

    Escalabilidade de conexoes

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,18 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class ConnectionFactory {
 	
 	public Connection recuperarConexao() {
 		
 		 try {
-	            return DriverManager
-	                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?createDatabaseIfNotExist=true&user=root&password=N@do1709");
+	            return createDataSource().getConnection();
 
 	        } catch (SQLException e) {
 	            System.out.println(e);
 	            throw new RuntimeException(e);
 	        }
 	}
+	
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank?createDatabaseIfNotExist=true&serverTimezone=America/Sao_Paulo&useSSl=false");
+        config.setUsername("root");
+        config.setPassword("N@do1709");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
+    }
 	 
 }
