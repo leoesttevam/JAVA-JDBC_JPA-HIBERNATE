e554844a24321d75c62376b755c11b71a58e94b0
leoesttevam <leonardoestevam17@gmail.com>
2023-08-29 13:38:17 -0300

    Isolando conexoes com factory

---
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.alura.bytebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public Connection recuperarConexao() {
+		
+		 try {
+	            return DriverManager
+	                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?createDatabaseIfNotExist=true&user=root&password=N@do1709");
+
+	        } catch (SQLException e) {
+	            System.out.println(e);
+	            throw new RuntimeException(e);
+	        }
+	}
+	 
+}
